f8748fb1d1972ac9f18c5be0e3481ceebd9f34b2
kai-niemi <kai.niemi.se@gmail.com>
2024-02-13 17:59:21 +0100

    hj

--- run-betting.sh
+++ run-betting.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#params="--spring.profiles.active=dev"
+params="--spring.profiles.active=dev"
 
 java -jar burpabet-betting-service/target/betting-service.jar $params $*
 
-#java -jar burpabet-betting-service/target/betting-service.jar \
 # --spring.profiles.active=local \
 # --spring.datasource.url="jdbc:postgresql://192.168.1.99:26257/burp_betting?sslmode=disable" \
 # --spring.datasource.username=root \
 # --spring.datasource.password= \
 # --spring.kafka.bootstrap-servers="192.168.1.99:9092" \
 # --spring.flyway.placeholders.cdc-sink-url="kafka://192.168.1.99:9092" \
+# --burp.customer-api-url=http://192.168.1.99:8090/api \
 #$*
--- run-wallet.sh
+++ run-wallet.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-#params="--spring.profiles.active=dev"
+params="--spring.profiles.active=dev"
 
 java -jar burpabet-wallet-service/target/wallet-service.jar $params $*
+
+# --spring.profiles.active=local \
+# --spring.datasource.url="jdbc:postgresql://192.168.1.99:26257/burp_customer?sslmode=disable" \
+# --spring.datasource.username=root \
+# --spring.datasource.password= \
+# --spring.kafka.bootstrap-servers="192.168.1.99:9092" \
+# --spring.flyway.placeholders.cdc-sink-url="kafka://192.168.1.99:9092" \
+#$*
