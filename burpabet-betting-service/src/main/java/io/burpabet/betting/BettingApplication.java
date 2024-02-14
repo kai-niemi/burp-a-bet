1a76887176902e115f5f413a4babe5fc49f2df38
kai-niemi <kai.niemi.se@gmail.com>
2024-02-14 11:58:10 +0100

    Tweak

--- burpabet-betting-service/src/main/java/io/burpabet/betting/BettingApplication.java
+++ burpabet-betting-service/src/main/java/io/burpabet/betting/BettingApplication.java
@@ -1,10 +1,9 @@
 package io.burpabet.betting;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-
+import ch.qos.logback.classic.Level;
 import org.jline.utils.AttributedString;
 import org.jline.utils.AttributedStyle;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jdbc.JdbcRepositoriesAutoConfiguration;
@@ -16,6 +15,9 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.core.annotation.Order;
 import org.springframework.shell.jline.InteractiveShellRunner;
 import org.springframework.shell.jline.PromptProvider;
+
+import java.util.Arrays;
+import java.util.LinkedList;
 
 @Configuration
 @ConfigurationPropertiesScan(basePackageClasses = BettingApplication.class)
@@ -46,12 +48,24 @@ public class BettingApplication implements PromptProvider {
         new SpringApplicationBuilder(BettingApplication.class)
                 .logStartupInfo(true)
                 .web(WebApplicationType.SERVLET)
-                .run(passThroughArgs.toArray(new String[] {}));
+                .run(passThroughArgs.toArray(new String[]{}));
     }
 
     @Override
     public AttributedString getPrompt() {
+        ch.qos.logback.classic.LoggerContext loggerContext = (ch.qos.logback.classic.LoggerContext) LoggerFactory
+                .getILoggerFactory();
+        ch.qos.logback.classic.Logger logger = loggerContext.getLogger("io.burpabet");
+        int fg =
+                switch (logger.getLevel().toInt()) {
+                    case Level.TRACE_INT -> AttributedStyle.MAGENTA;
+                    case Level.DEBUG_INT -> AttributedStyle.BLUE;
+                    case Level.INFO_INT -> AttributedStyle.GREEN;
+                    case Level.WARN_INT -> AttributedStyle.YELLOW;
+                    case Level.ERROR_INT -> AttributedStyle.RED;
+                    default -> AttributedStyle.GREEN;
+                };
         return new AttributedString("betting:$ ",
-                AttributedStyle.DEFAULT.foreground(AttributedStyle.GREEN));
+                AttributedStyle.DEFAULT.foreground(fg));
     }
 }
